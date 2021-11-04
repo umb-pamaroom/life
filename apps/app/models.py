c906893c8003db2c206af33c776cd20433c7ce2b
Avril Lavigne <avril1225happy@gmail.com>
2021-11-04 19:09:42 +0900

    no message

--- apps/app/models.py
+++ apps/app/models.py
@@ -21,7 +21,7 @@ class Condition(models.TextChoices):
 # Memoモデルを作成する
 class Memo(models.Model):
 
-    create_user = models.ForeignKey(User, related_name='relate_user', on_delete=models.CASCADE, blank=True, null=True)
+    create_user = models.ForeignKey(User, related_name='relate_user', on_delete=models.CASCADE)
 
     # 日付
     dateData = models.DateField(default=timezone.now, blank=True)
